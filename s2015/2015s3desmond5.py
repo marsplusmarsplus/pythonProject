c311a599543002ba004ce1f383d926b94a1b10ed
marsplusmarsplus <marsplus@hotmail.com>
2020-09-01 17:59:59 -0400

    a

--- s2015/2015s3desmond5.py
+++ s2015/2015s3desmond5.py
@@ -1,4 +1,4 @@
-data = open("s3.0a.in")
+data = open("s3.0b.in")
 maxParkedPlanes = 0
 numGates = int(data.readline().strip())
 numPlanes = int(data.readline().strip())
@@ -16,23 +16,24 @@ for i in range(numPlanes):
 print()
 gatesDecimalValue = 0
 for j in range(pow(numPlanes + 1, numGates)):  # MAIN LOOP
-    print("gates status = ", end=" ")
     temp = gatesDecimalValue
-    for ii in range(numGates - 1, -1, -1):
+    for ii in range(numGates - 1, -1, -1):  # park planes in gates
         gates[ii] = temp // pow(numPlanes + 1, ii)
         temp = temp - (temp // pow(numPlanes + 1, ii)) * pow(numPlanes + 1, ii)
+    void = 0
+    for hhh in gates:  # any duplicate planes?
+        if gates.count(hhh) > 1 and hhh != 0:
+            void = 1
+    print("void = " + str(void), end=" ")
     numParkedPlanes = 0
-    for iii in range(numGates):
+    print("gates status = ", end=" ")
+    for iii in range(numGates):  # how many planes were parked?
         print(str(gates[iii]), end=" ")
         if gates[iii] > 0:
             numParkedPlanes = numParkedPlanes + 1
     print("number of parked planes = " + str(numParkedPlanes))
-    if numParkedPlanes > maxParkedPlanes:
+    if numParkedPlanes > maxParkedPlanes and void == 0:
         maxParkedPlanes = numParkedPlanes
         print("max number of parked planes = " + str(maxParkedPlanes))
     gatesDecimalValue = gatesDecimalValue + 1
-    for hhh in gates:
-        if gates.count(hhh) > 1 and hhh != 0:
-            print("VOID", end=" ")
-    print()
 print("max number of parked planes = " + str(maxParkedPlanes))
